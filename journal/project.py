cac020d7fc00af638a2104eee5e6a5f64800edcd
Agustina Pesce <pesce.agustina@gmail.com>
2023-02-26 19:33:37 -0800

    Create a file with the functions used to create a new project file

---
+++ journal/project.py
@@ -0,0 +1,46 @@
+from utilities import get_parent_dir
+import os
+
+# Template for a new project
+PROJECT_TEMPLATE = """
+# Note of {project_name} project
+## {today}
+""".strip()
+
+
+def create_project(project_name, today):
+    """
+    Create a new project file if it doesn't exist.
+    If it exists, a new entry is created on it
+    """
+    # Set path to the project file
+    file_project = get_parent_dir("projects", today.year) / f"{project_name}.md"
+    # Create a new entry in the project file
+
+    # Create a new project file if it doesn't exit using the template
+    if not file_project.exists():
+        file_project.write_text(
+            PROJECT_TEMPLATE.format(project_name=project_name, today=today)
+        )
+    else:
+        # If the project file exist, it create a new entry on it.
+        # To do that, first save the file content and then write the new lines
+        # Save content of the file in a variable
+        content = file_project.read_text().split(sep="\n")
+        # If today's date doesn't exist, it is added to the file in a new line
+        if f"## {today}" not in content:
+            content = content[:2] + [f"## {today}", ""] + content[2:]
+            # Write the previous content in the file
+            file_project.write_text("\n".join(content))
+    return file_project
+
+
+def get_proyect_names(today):
+    """
+    Return a list with the name of all the project files that are in the project
+    directory.
+    """
+    # Set path to the project directory
+    project_path = get_parent_dir("project", today.year)
+    # Return a list with the project names
+    return os.listdir(project_path)
