e84e7a7a65a9b63ec7462dc2a700306855c5684a
Agustina Pesce <pesce.agustina@gmail.com>
2023-04-02 17:46:07 -0700

    Improve comments, docstings and help in the options

--- journal/manager.py
+++ journal/manager.py
@@ -1,49 +1,53 @@
 import click
-from utilities import date, get_parent_dir, open_editor, PATH_JOURNAL
-from project import create_project
+from utilities import dates, get_parent_dir, open_editor, PATH_JOURNAL
+from project import create_project, get_project_names
 from task import create_task
 
 
 @click.group()
 def journal():
     """
-    Open or create a task file or a project file.
-    All the file will be save it in `Documentos/journal`
+    CLI tool to create and open project and task files for journaling.
+    All files are saved in the `Documentos/journal` directory.
     """
-    # Create the journal directory
+    # Create the journal directory if it doesn't exist
     if not PATH_JOURNAL.exists():
         PATH_JOURNAL.mkdir(parents=True)
 
 
 @journal.command()
-@click.option("-n", "--name", help="Project name")
+@click.option("-n", "--name", help="Specify the name of the project to create or open.")
 @click.option(
     "-l",
     "--list_names",
     is_flag=True,
-    help="List the projects names existing in the directory",
+    help="List the name of all projects that exist in the directory.",
 )
 def project(name, list_names):
     """
     Create a new entry for a given project file or create it.
 
-    `name` option creates a new entry in the given project. If the file doesn't exist,
-    it is created.
-
-    `list_names` flag lists the different project names existing in the directory.
+    You have 2 options to choose: `--name` or `--list_name`.
+    - `--name` option requires specifying the project name.
+    - `--list_names` option will display a list of project names that exist in the directory.
     """
-    # # Set today's date and the start day of current and last week
-    # today, _, _ = date()
-    # # Create and open a project file or task file
-    # if name:
-    #     project = create_project(name, today)
-    #     open_editor(project)
-    # if list_names:
-    pass
+    # Get current date
+    today, _, _ = dates()
+    # List the names of the project in the directory
+
+    if list_names:
+        list_names = get_project_names(today)
+        print("Project names: \n", list_names)
+
+    # Create and open a project file
+    if name:
+        project = create_project(name, today)
+        open_editor(project)
 
 
 mensage_flag = (
-    "Open the task file for the current week along with the task file for last week"
+    "Open the task file for the current week along with the task file for last
+    week."
 )
 
 
@@ -53,13 +57,15 @@ def task(last_week):
     """
     Create a new task file for the current week.
 
-    `last_week` flag opens the task file for the current week along with
-    the task file for last week.
+    If --last_week option is used, it will open the task file for the current
+    week along with the file for the last week.
     """
-    # Set today's date and the start day of current and last week
+    # Get current date and the start day of current and last week
     today, start_current_week, start_last_week = dates()
-    # Create a new task
+
+    # Create a new task for the current week
     current_week_task = create_task(today, start_current_week)
+
     # If the flag is used, open the current week's file and last week's file
     if last_week:
         # Set the path to the last week's task file.
@@ -68,6 +74,12 @@ def task(last_week):
         )
         if last_week_task.exists():
             open_editor(current_week_task, last_week_task)
+        # If the last week's file doesn't exist, open only the file of the
+        # current week
+        else:
+            print("Doesn't exist a task file for the last week to show")
+            open_editor(current_week_task)
+    # If the flag isn't used, the task file of the current week is opened
     else:
         open_editor(current_week_task)
 
